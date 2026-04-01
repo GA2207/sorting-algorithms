863fa198971bfbb15c8fc1e39ab62f98ae7f278b
GA2207 <guillaume.averbouch@laplateforme.io>
2026-04-01 11:16:12 +0200

    fix: retrait de l'optimisation early stop du tri a bulles

--- sorting.py
+++ sorting.py
@@ -16,13 +16,9 @@ def bubble_sort(lst):
     arr = lst.copy()
     n = len(arr)
     for i in range(n):
-        swapped = False
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        if not swapped:
-            break
     return arr
 
 
